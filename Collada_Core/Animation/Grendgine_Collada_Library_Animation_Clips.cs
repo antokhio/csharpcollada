4654a0622facc8fe07f7e632cd9bc30f0564891e
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2023-08-23 14:19:12 -0500

    Fix spacing in Grendgine_Collada_Library_Animation_Clips.cs

--- Collada_Core/Animation/Grendgine_Collada_Library_Animation_Clips.cs
+++ Collada_Core/Animation/Grendgine_Collada_Library_Animation_Clips.cs
@@ -8,7 +8,6 @@ namespace grendgine_collada
 	[System.Xml.Serialization.XmlTypeAttribute(AnonymousType=true)]
 	public partial class Grendgine_Collada_Library_Animation_Clips
 	{
-
 		[XmlAttribute("id")]
 		public string ID;
 		
@@ -16,14 +15,13 @@ namespace grendgine_collada
 		public string Name;	
 		
 		
-	    [XmlElement(ElementName = "animation_clip")]
+		[XmlElement(ElementName = "animation_clip")]
 		public Grendgine_Collada_Animation_Clip[] Animation_Clip;	
 		
 		[XmlElement(ElementName = "asset")]
 		public Grendgine_Collada_Asset Asset;
 		
-	    [XmlElement(ElementName = "extra")]
+		[XmlElement(ElementName = "extra")]
 		public Grendgine_Collada_Extra[] Extra;	
 	}
 }
-
